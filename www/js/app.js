81cd93a3cfbf6066d745ca50aeb5d1ccca9c668a
Tyler Fisher <tyler@tylerjfisher.com>
2016-05-09 15:28:23 -0400

    basic audio-driven thing with multiple vr scenes

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,102 @@
 // Global jQuery references
+var $document;
+var $body;
+var $section;
+var $begin;
+var $audioPlayer;
+var $scenes;
+
+var NO_AUDIO = (window.location.search.indexOf('noaudio') >= 0);
+var CHECKPOINTS = [];
+var currentScene;
 
 /*
  * Run on page load.
  */
 var onDocumentLoad = function(e) {
     // Cache jQuery references
-    renderExampleTemplate();
-}
+    $document = $('document');
+    $body = $('body');
+    $section = $('.section');
+    $begin = $('.begin');
+    $audioPlayer = $('#audio-player');
+    $scenes = $('a-entity.scene');
 
-/*
- * Basic templating example.
- */
-var renderExampleTemplate = function() {
-    var context = $.extend(APP_CONFIG, {
-        'template_path': 'jst/example.html',
-        'config': JSON.stringify(APP_CONFIG, null, 4),
-        'copy': JSON.stringify(COPY, null, 4)
+    $begin.on('click', onBeginClick);
+
+    $section.css({
+        'opacity': 1,
+        'visibility': 'visible'
     });
 
-    var html = JST.example(context);
+    setupAudioPlayer();
+    buildCheckpoints();
+}
 
-    $('#template-example').html(html);
+var setupAudioPlayer = function() {
+    $audioPlayer.jPlayer({
+        loop: false,
+        supplied: 'mp3',
+        timeupdate: onTimeupdate,
+        volume: NO_AUDIO ? 0 : 1
+    });
+}
+
+var buildCheckpoints = function() {
+    for (var i = 0; i < $scenes.length; i++) {
+        var $scene = $scenes.eq(i);
+
+        var obj = {
+            'id': $scene.attr('id'),
+            'checkpoint': $scene.data('checkpoint')
+        }
+        CHECKPOINTS.push(obj);
+    }
+}
+
+var playAudio = function(audioURL) {
+    $audioPlayer.jPlayer('setMedia', {
+        mp3: audioURL
+    }).jPlayer('play');
+}
+
+var pauseAudio = function() {
+    $audioPlayer.jPlayer('pause');
+}
+
+var resumeAudio = function() {
+    $audioPlayer.jPlayer('play');
+}
+
+var onTimeupdate = function(e) {
+    var duration = e.jPlayer.status.duration;
+    var position = e.jPlayer.status.currentTime;
+
+
+    for (var i = 0; i < CHECKPOINTS.length; i++) {
+        var thisCheckpoint = CHECKPOINTS[i]
+        if (position < thisCheckpoint['checkpoint']) {
+            console.log(position, thisCheckpoint['checkpoint']);
+            if (thisCheckpoint['id'] === currentScene) {
+                console.log('break');
+                break;
+            } else {
+                for (var j = 0; j < CHECKPOINTS.length; j++) {
+                    if (CHECKPOINTS[j]['id'] !== thisCheckpoint['id']) {
+                        $('#' + CHECKPOINTS[j]['id']).attr('visible', 'false');
+                    } else {
+                        $('#' + CHECKPOINTS[j]['id']).attr('visible', 'true');
+                    }
+                }
+            }
+        }
+    }
+}
+
+var onBeginClick = function() {
+    $section.hide();
+    currentScene = $scenes.eq(0).attr('id');
+    playAudio('assets/test.mp3');
 }
 
 $(onDocumentLoad);
