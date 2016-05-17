c8dfb5c2dc40432ccec080d255cc3c84fbe0f303
Katie Park <katie.e.park@gmail.com>
2016-05-17 17:58:27 -0400

    Basically just the three.js demo

--- www/js/app.js
+++ www/js/app.js
@@ -1,226 +1,40 @@
-// Global jQuery references
-var $document;
-var $body;
-var $section;
-var $intro;
-var $vr;
-var $conclusion;
-var $begin;
-var $audioPlayer;
-var $playerWrapper;
-var $scenes;
-var $canvas;
-var $play;
-var $pause;
-var $returnButtons;
-var $sceneClose;
-
-var NO_AUDIO = (window.location.search.indexOf('noaudio') >= 0);
-var ASSETS_SLUG = APP_CONFIG.DEPLOYMENT_TARGET !== 'production' ? 'http://stage-apps.npr.org/' + APP_CONFIG.PROJECT_SLUG + '/assets/' : 'assets/'
-
-var CHECKPOINTS = [];
-var currentScene;
-
-/*
- * Run on page load.
- */
 var onDocumentLoad = function(e) {
-    // Cache jQuery references
-    $document = $('document');
-    $body = $('body');
-    $section = $('.section');
-    $intro = $('.intro');
-    $vr = $('.vr');
-    $conclusion = $('.conclusion');
-    $begin = $('.begin');
-    $audioPlayer = $('#audio-player');
-    $playerWrapper = $('.player-wrapper');
-    $scenes = $('a-entity.scene');
-    $play = $('.play');
-    $pause = $('.pause');
-    $returnButtons = $('.scene-buttons button')
-    $sceneClose = $('.scene-close');
-
-    $begin.on('click', onBeginClick);
-    $play.on('click', resumeAudio);
-    $pause.on('click', pauseAudio);
-    $returnButtons.on('click', onReturnButtonClick);
-    $sceneClose.on('click', onSceneCloseClick);
-
-    $section.css({
-        'opacity': 1,
-        'visibility': 'visible'
-    });
-
-    setupAudioPlayer();
-    buildCheckpoints();
-
     initGraphic();
 }
 
-var setupAudioPlayer = function() {
-    $audioPlayer.jPlayer({
-        loop: false,
-        supplied: 'mp3',
-        //timeupdate: onTimeupdate,
-        volume: NO_AUDIO ? 0 : 1
-    });
-}
-
-var buildCheckpoints = function() {
-    for (var i = 0; i < $scenes.length; i++) {
-        var $scene = $scenes.eq(i);
-
-        var obj = {
-            'id': $scene.attr('id'),
-            'checkpoint': $scene.data('checkpoint')
-        }
-        CHECKPOINTS.push(obj);
-    }
-}
-
-var playAudio = function(audioURL) {
-    $audioPlayer.jPlayer('setMedia', {
-        mp3: audioURL
-    }).jPlayer('play');
-    $play.hide();
-    $pause.show();
-}
-
-var pauseAudio = function() {
-    $audioPlayer.jPlayer('pause');
-    $pause.hide();
-    $play.show();
-}
-
-var resumeAudio = function() {
-    $audioPlayer.jPlayer('play');
-    $play.hide();
-    $pause.show();
-}
-
-var onTimeupdate = function(e) {
-    var duration = e.jPlayer.status.duration;
-    var position = e.jPlayer.status.currentTime;
-
-    for (var i = 0; i < CHECKPOINTS.length; i++) {
-        var thisCheckpoint = CHECKPOINTS[i]
-        if (position < thisCheckpoint['checkpoint'] && position > 0) {
-            if (thisCheckpoint['id'] === currentScene) {
-                break;
-            } else {
-                currentScene = thisCheckpoint['id'];
-                $canvas.velocity('fadeOut', {
-                    duration: 1000,
-                    complete: function() {
-                        showCurrentScene();
-
-                        $canvas.velocity('fadeIn', {
-                            duration: 1000,
-                            complete: function() {
-                                document.querySelector('#' + currentScene + ' .sky').emit('enter-scene');
-                                document.querySelector('#' + currentScene + ' .aux').emit('audio-play');
-                            }
-                        });
-                    }
-                });
-                break;
-            }
-        } else if (position > 50) {
-            exitFullscreen();
-            $vr.hide();
-            $conclusion.show();
-            $audioPlayer.jPlayer('stop');
-        }
-    }
-}
-
-var showCurrentScene = function() {
-    $scenes.find('.sky').attr('visible', 'false');
-    $('#' + currentScene).find('.sky').attr('visible', 'true');
-}
-
-var requestFullscreen = function() {
-    if (document.body.requestFullscreen) {
-        document.body.requestFullscreen();
-    } else if (document.body.mozRequestFullScreen) {
-        document.body.mozRequestFullScreen();
-    } else if (document.body.webkitRequestFullscreen) {
-        document.body.webkitRequestFullscreen();
-    } else if (document.body.msRequestFullscreen) {
-        document.body.msRequestFullscreen();
-    }
-}
-
-var exitFullscreen = function() {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-    } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-    }
-}
-
-var onBeginClick = function() {
-    //requestFullscreen();
-
-    $section.hide();
-    currentScene = $scenes.eq(0).attr('id');
-    showCurrentScene();
-    document.querySelector('#' + currentScene + ' .sky').emit('enter-scene');
-    document.querySelector('#' + currentScene + ' .aux').emit('audio-play');
-
-    playAudio(ASSETS_SLUG + 'test.mp3');
-
-    $canvas = $('canvas.a-canvas')
-}
-
-var onReturnButtonClick = function(e) {
-    var $this = $(this);
-    currentScene = $this.data('scene');
-    showCurrentScene();
-
-    requestFullscreen();
-
-    $conclusion.hide();
-    $playerWrapper.hide();
-    $vr.show();
-    $sceneClose.show();
-}
-
-var onSceneCloseClick = function() {
-    exitFullscreen();
-    $vr.hide();
-    $conclusion.show();
-}
-
-
 var initGraphic = function() {
-    var chartWidth = 90,
-        chartHeight = 30;
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    var xScale = d3.scale.linear()
-        .domain([0,50])
-        .range([0, chartWidth]);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-    var yScale = d3.scale.linear()
-        .domain([0,50])
-        .range([0, chartHeight]);
+    $('body').append(renderer.domElement);
 
-    //var columnMaterial = new THREE.MeshPhongMaterial({
-        //color: '#00CED1"
-    //});
+    var geometry = new THREE.BoxGeometry(1,1,1);
+    var material = new THREE.MeshPhongMaterial({ color: 0x40E0D0 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
-    //var columnGeo = new THREE.BoxGeometry(xScale(10), yScale(25), xScale(10));
-    //var columnMesh = new THREE.Mesh(columnGeo, columnMaterial);
+    var light = new THREE.AmbientLight('#999');
+    scene.add(light);
 
-    //columnMesh.position.set(xScale(25), yScale(10));
-    //$vr.find('a-scene')
-        //.append('
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    directionalLight.position.set(-2, 3, 10);
+    scene.add(directionalLight);
 
+    camera.position.z = 5;
+
+    function render() {
+        requestAnimationFrame(render);
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+    }
+
+    render();
 };
 
 $(onDocumentLoad);
