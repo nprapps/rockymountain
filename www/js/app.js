a4a9c12c0416cb04e06bea62cbdd1a6deb3d086a
Katie Park <katie.e.park@gmail.com>
2016-05-18 17:33:53 -0400

    Getting rid of the nonworking SVG code

--- www/js/app.js
+++ www/js/app.js
@@ -10,7 +10,7 @@ var initGraphic = function() {
     camera.position.z = 25;
 
     // Add elements to an Object3D
-    //var groupObject = createObject3D();
+    var groupObject = createObject3D();
     //scene.add(groupObject);
 
     var lineObject = addLine()
@@ -30,7 +30,7 @@ var initGraphic = function() {
         //groupObject.rotation.y += 0.01;
 
         //lineObject.rotation.x += 0.005;
-        lineObject.rotation.y += 0.005;
+        //lineObject.rotation.y += 0.005;
 
         //shape.rotation.x += 0.005;
         //shape.rotation.y += 0.005;
@@ -55,34 +55,6 @@ var setupLights = function(scene) {
     var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
     directionalLight.position.set(-2, 3, 10);
     scene.add(directionalLight);
-};
-
-var createShape = function( shape, color, x, y, z, rx, ry, rz, s ) {
-    // flat shape
-
-    var geometry = new THREE.ShapeGeometry( shape );
-    var material = new THREE.MeshPhongMaterial({
-        color: color,
-        side: THREE.DoubleSide,
-        overdraw: true
-    });
-
-    var mesh = new THREE.Mesh( geometry, material );
-    mesh.position.set( x, y, z );
-    mesh.rotation.set( rx, ry, rz );
-    mesh.scale.set( s, s, s );
-
-    return mesh;
-};
-
-var addShape = function() {
-    var svgPath = "M805.8,250.8L803.9,249L802.7,248.3L804.1,246.3L807,248.2L805.8,250.8z";
-
-    var shape = transformSVGPath(svgPath);
-    var shapeMesh = createShape(shape, '#ffcc66', 0, 0, 0, Math.PI, 0, 0, 1);
-
-    console.log(shape, shapeMesh);
-    return shapeMesh;
 };
 
 var addLine = function() {
@@ -247,7 +219,7 @@ var getLineFromPoints = function(points, color) {
 
 var getPointsFrom2DArray = function(shapeArray, size) {
     var points = [];
-    
+
     // Create d3 scales
     var xExtent = d3.extent(shapeArray, function(d) { return d[0]; }),
         yExtent = d3.extent(shapeArray, function(d) { return d[1]; }),
@@ -260,9 +232,6 @@ var getPointsFrom2DArray = function(shapeArray, size) {
     var yScale = d3.scale.linear()
         .domain(yExtent)
         .range([heightRatio * size/2, -heightRatio * size/2]);
-
-    console.log(xExtent, yExtent);
-    console.log(xScale, yScale);
 
     for (var i=0; i<shapeArray.length; i++) {
         var point = shapeArray[i];
@@ -307,234 +276,5 @@ var createObject3D = function() {
 
     return groupObject;
 };
-
-// Borrowed from Gabriel Florit
-function transformSVGPath(pathStr) {
-
-  const DIGIT_0 = 48, DIGIT_9 = 57, COMMA = 44, SPACE = 32, PERIOD = 46,
-      MINUS = 45;
-
-  var path = new THREE.Shape();
-
-  var idx = 1, len = pathStr.length, activeCmd,
-      x = 0, y = 0, nx = 0, ny = 0, firstX = null, firstY = null,
-      x1 = 0, x2 = 0, y1 = 0, y2 = 0,
-      rx = 0, ry = 0, xar = 0, laf = 0, sf = 0, cx, cy;
-
-  function eatNum() {
-    var sidx, c, isFloat = false, s;
-    // eat delims
-    while (idx < len) {
-      c = pathStr.charCodeAt(idx);
-      if (c !== COMMA && c !== SPACE)
-        break;
-      idx++;
-    }
-    if (c === MINUS)
-      sidx = idx++;
-    else
-      sidx = idx;
-    // eat number
-    while (idx < len) {
-      c = pathStr.charCodeAt(idx);
-      if (DIGIT_0 <= c && c <= DIGIT_9) {
-        idx++;
-        continue;
-      }
-      else if (c === PERIOD) {
-        idx++;
-        isFloat = true;
-        continue;
-      }
-
-      s = pathStr.substring(sidx, idx);
-      return isFloat ? parseFloat(s) : parseInt(s);
-    }
-
-    s = pathStr.substring(sidx);
-    return isFloat ? parseFloat(s) : parseInt(s);
-  }
-
-  function nextIsNum() {
-    var c;
-    // do permanently eat any delims...
-    while (idx < len) {
-      c = pathStr.charCodeAt(idx);
-      if (c !== COMMA && c !== SPACE)
-        break;
-      idx++;
-    }
-    c = pathStr.charCodeAt(idx);
-    return (c === MINUS || (DIGIT_0 <= c && c <= DIGIT_9));
-  }
-
-  var canRepeat;
-  activeCmd = pathStr[0];
-  while (idx <= len) {
-    canRepeat = true;
-    switch (activeCmd) {
-        // moveto commands, become lineto's if repeated
-      case 'M':
-        x = eatNum();
-        y = eatNum();
-        path.moveTo(x, y);
-        activeCmd = 'L';
-        break;
-      case 'm':
-        x += eatNum();
-        y += eatNum();
-        path.moveTo(x, y);
-        activeCmd = 'l';
-        break;
-      case 'Z':
-      case 'z':
-        canRepeat = false;
-        if (x !== firstX || y !== firstY)
-          path.lineTo(firstX, firstY);
-        break;
-        // - lines!
-      case 'L':
-      case 'H':
-      case 'V':
-        nx = (activeCmd === 'V') ? x : eatNum();
-        ny = (activeCmd === 'H') ? y : eatNum();
-        path.lineTo(nx, ny);
-        x = nx;
-        y = ny;
-        break;
-      case 'l':
-      case 'h':
-      case 'v':
-        nx = (activeCmd === 'v') ? x : (x + eatNum());
-        ny = (activeCmd === 'h') ? y : (y + eatNum());
-        path.lineTo(nx, ny);
-        x = nx;
-        y = ny;
-        break;
-        // - cubic bezier
-      case 'C':
-        x1 = eatNum(); y1 = eatNum();
-      case 'S':
-        if (activeCmd === 'S') {
-          x1 = 2 * x - x2; y1 = 2 * y - y2;
-        }
-        x2 = eatNum();
-        y2 = eatNum();
-        nx = eatNum();
-        ny = eatNum();
-        path.bezierCurveTo(x1, y1, x2, y2, nx, ny);
-        x = nx; y = ny;
-        break;
-      case 'c':
-        x1 = x + eatNum();
-        y1 = y + eatNum();
-      case 's':
-        if (activeCmd === 's') {
-          x1 = 2 * x - x2;
-          y1 = 2 * y - y2;
-        }
-        x2 = x + eatNum();
-        y2 = y + eatNum();
-        nx = x + eatNum();
-        ny = y + eatNum();
-        path.bezierCurveTo(x1, y1, x2, y2, nx, ny);
-        x = nx; y = ny;
-        break;
-        // - quadratic bezier
-      case 'Q':
-        x1 = eatNum(); y1 = eatNum();
-      case 'T':
-        if (activeCmd === 'T') {
-          x1 = 2 * x - x1;
-          y1 = 2 * y - y1;
-        }
-        nx = eatNum();
-        ny = eatNum();
-        path.quadraticCurveTo(x1, y1, nx, ny);
-        x = nx;
-        y = ny;
-        break;
-      case 'q':
-        x1 = x + eatNum();
-        y1 = y + eatNum();
-      case 't':
-        if (activeCmd === 't') {
-          x1 = 2 * x - x1;
-          y1 = 2 * y - y1;
-        }
-        nx = x + eatNum();
-        ny = y + eatNum();
-        path.quadraticCurveTo(x1, y1, nx, ny);
-        x = nx; y = ny;
-        break;
-        // - elliptical arc
-      case 'A':
-        rx = eatNum();
-        ry = eatNum();
-        xar = eatNum() * DEGS_TO_RADS;
-        laf = eatNum();
-        sf = eatNum();
-        nx = eatNum();
-        ny = eatNum();
-        if (rx !== ry) {
-          console.warn("Forcing elliptical arc to be a circular one :(",
-                       rx, ry);
-        }
-        // SVG implementation notes does all the math for us! woo!
-        // http://www.w3.org/TR/SVG/implnote.html#ArcImplementationNotes
-        // step1, using x1 as x1'
-        x1 = Math.cos(xar) * (x - nx) / 2 + Math.sin(xar) * (y - ny) / 2;
-        y1 = -Math.sin(xar) * (x - nx) / 2 + Math.cos(xar) * (y - ny) / 2;
-        // step 2, using x2 as cx'
-        var norm = Math.sqrt(
-          (rx*rx * ry*ry - rx*rx * y1*y1 - ry*ry * x1*x1) /
-          (rx*rx * y1*y1 + ry*ry * x1*x1));
-        if (laf === sf)
-          norm = -norm;
-        x2 = norm * rx * y1 / ry;
-        y2 = norm * -ry * x1 / rx;
-        // step 3
-        cx = Math.cos(xar) * x2 - Math.sin(xar) * y2 + (x + nx) / 2;
-        cy = Math.sin(xar) * x2 + Math.cos(xar) * y2 + (y + ny) / 2;
-
-        var u = new THREE.Vector2(1, 0),
-            v = new THREE.Vector2((x1 - x2) / rx,
-                                  (y1 - y2) / ry);
-        var startAng = Math.acos(u.dot(v) / u.length() / v.length());
-        if (u.x * v.y - u.y * v.x < 0)
-          startAng = -startAng;
-
-        // we can reuse 'v' from start angle as our 'u' for delta angle
-        u.x = (-x1 - x2) / rx;
-        u.y = (-y1 - y2) / ry;
-
-        var deltaAng = Math.acos(v.dot(u) / v.length() / u.length());
-        // This normalization ends up making our curves fail to triangulate...
-        if (v.x * u.y - v.y * u.x < 0)
-          deltaAng = -deltaAng;
-        if (!sf && deltaAng > 0)
-          deltaAng -= Math.PI * 2;
-        if (sf && deltaAng < 0)
-          deltaAng += Math.PI * 2;
-
-        path.absarc(cx, cy, rx, startAng, startAng + deltaAng, sf);
-        x = nx;
-        y = ny;
-        break;
-      default:
-        throw new Error("weird path command: " + activeCmd);
-    }
-    if (firstX === null) {
-      firstX = x;
-      firstY = y;
-    }
-    // just reissue the command
-    if (canRepeat && nextIsNum())
-      continue;
-    activeCmd = pathStr[idx++];
-  }
-
-  return path;
-}
 
 $(onDocumentLoad);
