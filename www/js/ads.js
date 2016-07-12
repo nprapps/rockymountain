8feae16a8bbee10068b1a9df509b6befbbdaa0b5
Tyler Fisher <tyler@tylerjfisher.com>
2016-07-12 10:45:38 -0400

    integrate ad code into our code

---
+++ www/js/ads.js
@@ -0,0 +1,31 @@
+var googletag = googletag || {};
+googletag.cmd = googletag.cmd || [];
+(function() {
+  var gads = document.createElement('script');
+  gads.async = true;
+  gads.type = 'text/javascript';
+  var useSSL = 'https:' == document.location.protocol;
+  gads.src = (useSSL ? 'https:' : 'http:') +
+    '//www.googletagservices.com/tag/js/gpt.js';
+  var node = document.getElementsByTagName('script')[0];
+  node.parentNode.insertBefore(gads, node);
+})();
+
+var firstSlot;
+var secondSlot;
+googletag.cmd.push(function() {
+    firstSlot = googletag.defineSlot('/6735/6735.npr.viz/national_park_vr', [301, 270], 'subaru1').addService(googletag.pubads());
+    secondSlot = googletag.defineSlot('/6735/6735.npr.viz/national_park_vr', [302, 270], 'subaru2').addService(googletag.pubads());
+
+    googletag.pubads().enableSingleRequest();
+    googletag.pubads().collapseEmptyDivs();
+    googletag.pubads().disableInitialLoad();
+    googletag.enableServices();
+});
+
+function refreshSecondSlot() {
+    googletag.cmd.push(function() {
+        googletag.pubads().refresh([secondSlot]);
+    });
+    return false;
+}
